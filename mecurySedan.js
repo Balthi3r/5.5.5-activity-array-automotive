80ec4c3ff84934f3acba8051e2bc80220d31cb61
balthier <rrodrijose@gmail.com>
2022-12-09 22:39:30 -0600

    done

--- mecurySedan.js
+++ mecurySedan.js
@@ -14,7 +14,7 @@ class Car extends VehicleModule{
         this.fuel=10;
         this.scheduleService=False;
     }
-}
+
 
 checkService(){
     if(this.milage>30000){
@@ -25,10 +25,28 @@ checkService(){
 
 Start(){
     if(this.fuel>0){
-        console.log("car is on")
+        console.log("car is on");
         return this.started=true
     }else{
-        console.log("need fuel")
+        console.log("need fuel");
         return this.started=false;
     }
 }
+loadPassenger(num){
+    if(this .passenger<this.maxPassangers){
+        if((num+this.passenger)<=this.maxPassangers){
+            this.passenger=num;
+        }else{
+            console.log(this.model+" "+this.make+ " not alot of space.");
+        }
+    }else{
+        console.log(this.model+ " "+this.make+ " is full");
+    }
+}
+
+let myCar= new Car('mercury','rad_sedan','2002','white',50000)
+myCar.start()
+myCar.loadPAssenger(5)
+myCar.stop()
+myCar.checkService()
+console.log(myCar)
